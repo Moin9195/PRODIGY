163dfdaece33586c92d902bfe9a1c866f31b6362
Moin Ajaz Shaikh <139529798+Moin9195@users.noreply.github.com>
2023-11-03 20:21:53 +0530

    Add files via upload

---
+++ prodigy_task1/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  /* Change the color on hover */
+  .navbar-nav .nav-link:hover {
+    color: #FFA500; /* Change this to the desired hover color */
+  }
+
+  /* Change the color when clicked */
+  .navbar-nav .nav-link:focus,
+  .navbar-nav .nav-link:active {
+    color: #FF4500; /* Change this to the desired active (clicked) color */
+  }
+
+</style>
+</head>
+<body style="height:1500px">
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <div class="container-fluid">
+    <div class="container-fluid">
+    <!-- Links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="header.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="#">Link 3</a>
+      </li> -->
+    </ul>
+  </div>
+  </div>
+</nav>
+
+<div class="container-fluid" style="margin-top:80px">
+ 
+</div>
+
+</body>
+</html>
+
+
